8fa10589ac6fe20f75d9c063a0249d178c0066ea
Artur <arturkolozyan19122006@gmail.com>
2025-04-16 16:49:42 +0300

    structure, sqlalchemy models, config, main.py

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-import aiohttp
 import asyncio
+from aiogram import Bot
 
-async def fetch_data(url: str):
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                response.raise_for_status()
-                return await response.json()
-    except aiohttp.ClientError as e:
-        print(f"Ошибка при запросе к API: {e}")
-        return None
-    except asyncio.TimeoutError:
-        print("Превышено время ожидания запроса к API.")
-        return None
+from config import Config
+
+config = Config()
+
 
 async def main():
-    data = await fetch_data("https://jsonplaceholder.typicode.com/users")
-    if data:
-        print(data)
+    bot = Bot(token=config.get_bot_token())
+
 
 if __name__ == "__main__":
     asyncio.run(main())
